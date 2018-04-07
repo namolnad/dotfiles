85a06d745dd22e24c0226f314c5004e47b7a7107
Dan L <daniel.h.loman@gmail.com>
2018-04-07 11:48:06 -0700

    Add keybindings config; hub pr setup

--- .oh-my-zsh/custom/dans_shortcuts-and-improvements.zsh
+++ .oh-my-zsh/custom/dans_shortcuts-and-improvements.zsh
@@ -8,6 +8,7 @@ alias xcode_clean="rm -rf ~/Library/Developer/Xcode/DerivedData/* && rm -rf ~/Li
 #   9. GIT
 #   ---------------------------
 
+alias git=hub
 alias gfpa="git fetch --prune --all"
 alias gitlog="git log -15 --oneline --reverse"
 alias mergetool="git mergetool -t opendiff"
@@ -15,6 +16,8 @@ alias gitstat="git status -sb"
 alias grbo="git rebase-origin" # Git rebase origin, -fp flag pushes to origin w/ flag --force-with-lease
 alias gplease="git push origin --force-with-lease"
 alias gdp="git diff HEAD^ HEAD" # Git diff previous commit
+alias gpr-ios='git pull-request -F ~/Code/Instacart-ios/.github/PULL_REQUEST_TEMPLATE -o -e -c'
+alias gpr-list='g pr list -f "%sC%>(8)%i%Creset  %t% l -----% au  %n"'
 
 #   ---------------------------
 #   11. EDITORS
@@ -40,7 +43,9 @@ export PATH="/usr/local/sbin:$PATH"
 
 alias lg=". ~/code/dans-scripts/daily_work_log.sh"
 
-standup() { ( . /usr/bin/cd ~/code; git standup "$@"; ) }
+standup() { 
+  ( . /usr/bin/cd ~/code; git standup "$@"; ) 
+}
 
 start_pianobar() {
   echo 'Do you want to start pianobar? [y/N]'
