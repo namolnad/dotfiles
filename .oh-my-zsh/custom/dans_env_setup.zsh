039760042e87c94696e25cac190b4b03a0202416
Dan L <daniel.h.loman@gmail.com>
2018-07-19 19:59:45 -0700

    +default slugs to env. Better Instacart env org

--- .oh-my-zsh/custom/dans_env_setup.zsh
+++ .oh-my-zsh/custom/dans_env_setup.zsh
@@ -6,12 +6,6 @@ export INSTACART_LAPTOP='69733248'
 export INSTACART_THUNDERBOLT_DISPLAY='69508137'
 export HOME_IMAC='69678529'
 
-# SLACK (for iOS deploys)
-export SLACK_URL="https://hooks.slack.com/services/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"
-
-#Fastlane
-export FASTLANE_USERNAME='apple@instacart.com'
-
 for f in $(find $HOME/.custom_credentials -type f); do
   source $f
 done
--- .oh-my-zsh/custom/instacart.zsh
+++ .oh-my-zsh/custom/instacart.zsh
@@ -3,29 +3,31 @@
 #   ---------------------------
 
 export SUDO_USER='dan.loman'
+export DEFAULT_COUNTRY_SLUG='US'
+export DEFAULT_PBI_SLUG='instacart'
 
-alias ishopper="cd ~/Code/Instashopper-ios;pwd"
-alias ashopper="cd ~/Code/Instashopper-android;pwd"
+# General
+alias vpn-disconnect="osascript -e 'tell application \"/Applications/Tunnelblick.app\"' -e 'disconnect \"Instacart\"' -e 'end tell'"
+alias vpn-connect="osascript -e 'tell application \"/Applications/Tunnelblick.app\"' -e 'connect \"Instacart\"' -e 'end tell'"
+
+# iOS
 alias iconsumer="cd ~/Code/Instacart-ios;pwd"
+alias reload_iconsumer="((killall Xcode || true); iconsumer; xed .)"
+alias carthage_publish="AWS_PROFILE=default AWS_REGION='us-east-1' bundle exec carthage_cache publish -b instacart-ios-consumer-cache --trace"
+alias carthage_force_install="AWS_PROFILE=default AWS_REGION='us-east-1' bundle exec carthage_cache install -b instacart-ios-consumer-cache --trace --force"
+
+# Rails
 alias instacart="cd ~/Code/Carrot/customers/instacart;pwd"
-alias railss="cd ~/Code/Carrot/customers/instacart;pwd;myip;echo foreman run rails s -b 0.0.0.0;foreman run rails s -b 0.0.0.0"
-alias shoppers="cd ~/Code/Carrot/shoppers/shopper-api;pwd;myip;echo bundle exec rails server -b 0.0.0.0 -p 5000;bundle exec rails server -b 0.0.0.0 -p 5000"
+alias railss="cd ~/Code/Carrot/customers/instacart;pwd;myip;echo foreman run rails s -b 0.0.0.0;rails s -b 0.0.0.0"
+alias monitor-deploy="while :; do echo && isc deploys all-customers --count 1 && echo && sleep 60; done"
+# Rails (console)
 alias railsc="cd ~/Code/Carrot/customers/instacart;pwd;echo foreman run rails console;foreman run rails console"
-alias railsm="cd ~/Code/Carrot/migrations;pwd;echo bundle exec rake db:migrate;bundle exec rake db:migrate"
-alias clipscript="cat ~dans-scripts/work/isc-scripts.rb | pbcopy"
-alias localsync="instacart;pgsync zones,warehouse_zones,regions,warehouses,delivery_hours,applicant_variants,applicant_experiments,postal_codes;pgsync users \"where email='dan.loman@instacart.com'\""
-alias identity-permission-fix="ssh-add -A"
-alias rs="psql -h sherlock.cwripsh2edcd.us-east-1.redshift.amazonaws.com -U danloman -d analytics -p 5439"
-alias carthage_publish="AWS_PROFILE=default AWS_REGION="us-east-1" bundle exec carthage_cache publish -b instacart-ios-consumer-cache --trace"
-alias carthage_force_install="AWS_PROFILE=default AWS_REGION="us-east-1" bundle exec carthage_cache install -b instacart-ios-consumer-cache --trace --force"
 alias production_console="(instacart && script/console --country US --pbi instacart --server production)"
 alias staging_console="(instacart && script/console --country US --pbi instacart --server console.staging)"
-alias vpn-connect="osascript -e 'tell application \"/Applications/Tunnelblick.app\"' -e 'connect \"Instacart\"' -e 'end tell'"
-alias vpn-disconnect="osascript -e 'tell application \"/Applications/Tunnelblick.app\"' -e 'disconnect \"Instacart\"' -e 'end tell'"
-alias reload_iconsumer="((killall Xcode || true); iconsumer; xed .)"
-alias monitor-deploy="while :; do echo && isc deploys all-customers --count 1 && echo && sleep 60; done"
-alias console-staging="(instacart; script/console --country US --pbi instacart --server console.instacart.customers-stg)"
-alias console-prod="(instacart; script/console --country US --pbi instacart --server console.instacart.customers)"
+# Rails (db)
+alias localsync="instacart;pgsync zones,warehouse_zones,regions,warehouses,delivery_hours,applicant_variants,applicant_experiments,postal_codes;pgsync users \"where email='dan.loman@instacart.com'\""
+alias railsm="cd ~/Code/Carrot/migrations;pwd;echo bundle exec rake db:migrate;bundle exec rake db:migrate"
+alias rs="psql -h sherlock.cwripsh2edcd.us-east-1.redshift.amazonaws.com -U danloman -d analytics -p 5439"
 
 kill_old_server() {
   #!/bin/bash
