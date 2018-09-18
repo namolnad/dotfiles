ebd25ad22404c761cfb88b9000e8f50459440dde
Dan L <daniel.h.loman@gmail.com>
2018-09-18 13:15:15 -0700

    Fix cd implementation which calls ls. Add github to VSCode user settings

--- .oh-my-zsh/custom/terminal-improvements.zsh
+++ .oh-my-zsh/custom/terminal-improvements.zsh
@@ -8,7 +8,7 @@ alias cp='cp -iv'                           # Preferred 'cp' implementation
 alias mv='mv -iv'                           # Preferred 'mv' implementation
 alias mkdir='mkdir -pv'                     # Preferred 'mkdir' implementation
 alias less='less -FSRXc'                    # Preferred 'less' implementation
-cd() { builtin cd "$@"; lc; }               # Always list directory contents upon 'cd'
+cd() { builtin cd "$@"; ls; }               # Always list directory contents upon 'cd'
 alias cd..='cd ../'                         # Go back 1 directory level (for fast typers)
 alias ..='cd ../'                           # Go back 1 directory level
 alias ...='cd ../../'                       # Go back 2 directory levels
