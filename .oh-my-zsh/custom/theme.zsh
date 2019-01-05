4dd30d02f1b3d9ef422c8b4edbff17b4bbb49369
Dan L <daniel.h.loman@gmail.com>
2019-01-04 20:30:55 -0800

    Add cursor shape customization for vi-mode

--- .oh-my-zsh/custom/theme.zsh
+++ .oh-my-zsh/custom/theme.zsh
@@ -1,3 +1,30 @@
+# Updates editor information when the keymap changes.
+
+function zle-keymap-select zle-line-init {
+    # change cursor shape in iTerm2
+    case $KEYMAP in
+        vicmd)      print -n -- "\E]50;CursorShape=0\C-G";;  # block cursor
+        viins|main) print -n -- "\E]50;CursorShape=1\C-G";;  # line cursor
+    esac
+
+    zle reset-prompt
+    zle -R
+}
+
+function zle-line-finish {
+    print -n -- "\E]50;CursorShape=0\C-G"  # block cursor
+}
+
+zle -N zle-line-init
+zle -N zle-line-finish
+zle -N zle-keymap-select
+
+# function zsh_vi_mode() {
+#     case $KEYMAP in
+#         (viins|main) echo -n "INSERT";;
+#         (vicmd|*)      echo -n "-- NORMAL --";;
+#     esac
+# }
 
 #   ---------------------------
 #   ZSH THEME
@@ -19,6 +46,8 @@ POWERLEVEL9K_CUSTOM_INTERNET_SIGNAL="zsh_internet_signal"
 
 POWERLEVEL9K_VCS_MODIFIED_BACKGROUND='yellow'
 POWERLEVEL9K_VCS_UNTRACKED_BACKGROUND='yellow'
+
+# POWERLEVEL9K_CUSTOM_VI_MODE="zsh_vi_mode"
 
 POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(battery context dir vcs)
 POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status time background_jobs rbenv)
