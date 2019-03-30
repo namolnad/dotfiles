daea599f73b9476c9455da5ecda2c8cef2d0c55f
Dan Loman <daniel.h.loman@gmail.com>
2019-03-30 14:49:04 -0700

    Move FZF to dans_shortchuts_and_improvements

--- .oh-my-zsh/custom/dans_shortcuts-and-improvements.zsh
+++ .oh-my-zsh/custom/dans_shortcuts-and-improvements.zsh
@@ -106,6 +106,20 @@ function cheat() {
 }
 
 #   ---------------------------
+#   FZF SETUP
+#   ---------------------------
+
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+# export FZF_DEFAULT_COMMAND='fd --type f -d 15'
+export FZF_DEFAULT_COMMAND='ag --hidden -l --depth 5 --ignore .git --nocolor -g ""'
+# export FZF_DEFAULT_COMMAND='ag %s -l --depth 5 --nocolor -g ""'
+# export FZF_DEFAULT_COMMAND='ag --hidden --ignore .git -g ""'
+# export FZF_DEFAULT_COMMAND="find . -maxdepth 5 -type f -not -path '*/\.git/*'"
+# export FZF_DEFAULT_COMMAND="find . -maxdepth 5 -not -path '*/\.git/*'"
+export FZF_COMMAND_T="$FZF_DEFAULT_COMMAND"
+export FZF_DEFAULT_OPS='--extended'
+
+#   ---------------------------
 #   KEY BINDINGS
 #   ---------------------------
 
