c15361b8b3d215be3b724f8eaf8afe288f00cb7d
Dan Loman <daniel.h.loman@gmail.com>
2019-09-17 17:05:42 -0700

    Add Go configuration

--- .omz_custom.symlink/dans_env_setup.zsh
+++ .omz_custom.symlink/dans_env_setup.zsh
@@ -5,7 +5,12 @@ for f in $(find $HOME/.custom_credentials -type f); do
   source "$f"
 done
 
-# Modify PATH
+# GO
+export GOPATH=$HOME/.go
+export GOROOT=/usr/local/opt/go/libexec
+export PATH=$PATH:$GOPATH/bin:$GOROOT/bin
+
+# Modify PATH for imagmagick
 export PATH="$PATH:/usr/local/opt/imagemagick@6/bin:/usr/local/sbin"
 
 iterm2_print_user_vars() {
