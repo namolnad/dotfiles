a1dc74c4dd84b17263f1a00d8ae50ba35293b113
Dan Loman <daniel.h.loman@gmail.com>
2024-03-19 11:20:10 -0700

    Set up trouble a bit more

--- .config/nvim/lua/namolnad/lazy/trouble.lua
+++ .config/nvim/lua/namolnad/lazy/trouble.lua
@@ -1,23 +1,23 @@
 return {
-    {
-        "folke/trouble.nvim",
-        config = function()
-            require("trouble").setup({
-                icons = false,
-            })
+  {
+    "folke/trouble.nvim",
+    config = function()
+      local trouble = require("trouble")
 
-            vim.keymap.set("n", "<leader>tt", function()
-                require("trouble").toggle()
-            end)
+      trouble.setup({
+        icons = true,
+      })
 
-            vim.keymap.set("n", "[t", function()
-                require("trouble").next({skip_groups = true, jump = true});
-            end)
+      vim.keymap.set("n", "<leader>tt", function() trouble.toggle() end)
+      vim.keymap.set("n", "<leader>tw", function() trouble.toggle("workspace_diagnostics") end)
+      vim.keymap.set("n", "<leader>td", function() trouble.toggle("document_diagnostics") end)
+      vim.keymap.set("n", "<leader>tq", function() trouble.toggle("quickfix") end)
+      vim.keymap.set("n", "<leader>tl", function() trouble.toggle("loclist") end)
 
-            vim.keymap.set("n", "]t", function()
-                require("trouble").previous({skip_groups = true, jump = true});
-            end)
+      vim.keymap.set("n", "gR", function() trouble.toggle("lsp_references") end)
 
-        end
-    },
+      vim.keymap.set("n", "[t", function() trouble.next({ skip_groups = true, jump = true }); end)
+      vim.keymap.set("n", "]t", function() trouble.previous({ skip_groups = true, jump = true }); end)
+    end
+  },
 }
