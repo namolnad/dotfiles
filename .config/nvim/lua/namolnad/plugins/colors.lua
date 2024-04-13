df615659214558b5e4b0ba18a52db5a971435e4a
Dan Loman <daniel.h.loman@gmail.com>
2024-04-12 22:32:42 -0700

    Adjust theme colors (mostly status line highlight group)

--- .config/nvim/lua/namolnad/plugins/colors.lua
+++ .config/nvim/lua/namolnad/plugins/colors.lua
@@ -13,19 +13,20 @@ return {
       require('tokyonight').setup {
         -- your configuration comes here
         -- or leave it empty to use the default settings
-        style = 'storm', -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+        style = 'moon', -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
         transparent = true, -- Enable this to disable setting the background color
         terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
         styles = {
           -- Style to be applied to different syntax groups
           -- Value is any valid attr-list value for `:help nvim_set_hl`
-          comments = { italic = false },
+          comments = { italic = true },
           keywords = { italic = false },
           -- Background styles. Can be "dark", "transparent" or "normal"
           sidebars = 'dark', -- style for sidebars, see below
           floats = 'dark', -- style for floating windows
         },
       }
+      -- vim.cmd 'colorscheme tokyonight'
     end,
   },
 
@@ -35,6 +36,12 @@ return {
     config = function()
       require('rose-pine').setup {
         disable_background = true,
+        variant = 'moon',
+        highlight_groups = {
+          TelescopeBorder = { fg = 'rose', bg = 'rose', blend = 10 },
+          MiniStatuslineFilename = { fg = 'foam', bg = 'foam', blend = 10 },
+          MiniStatuslineDevinfo = { fg = 'foam', bg = 'foam', blend = 10 },
+        },
       }
 
       vim.cmd 'colorscheme rose-pine'
