4f4498d8b82957a817b5f4b1e47add9722ee99d9
Dan Loman <daniel.h.loman@gmail.com>
2024-09-16 07:58:25 -0700

    Add nvim/split panes handling for wezterm

--- config/.wezterm.lua
+++ config/.wezterm.lua
@@ -31,63 +31,6 @@ custom_color_scheme.brights = {
 
 local config = wezterm.config_builder()
 
-config.keys = {
-	{
-		key = "h",
-		mods = "CTRL",
-		action = act.ActivatePaneDirection("Left"),
-	},
-	{
-		key = "j",
-		mods = "CTRL",
-		action = act.ActivatePaneDirection("Down"),
-	},
-	{
-		key = "k",
-		mods = "CTRL",
-		action = act.ActivatePaneDirection("Up"),
-	},
-	{
-		key = "l",
-		mods = "CTRL",
-		action = act.ActivatePaneDirection("Right"),
-	},
-	{
-		key = "w",
-		mods = "CTRL",
-		action = act({ CloseCurrentPane = { confirm = true } }),
-	},
-	{
-		key = "k",
-		mods = "SUPER",
-		action = act.Multiple({
-			act.ClearScrollback("ScrollbackAndViewport"),
-			act.SendKey({ key = "L", mods = "CTRL" }),
-		}),
-	},
-	{
-		key = "1",
-		mods = "SUPER",
-		action = act.SwitchToWorkspace({
-			name = "editor",
-		}),
-	},
-	{
-		key = "2",
-		mods = "SUPER",
-		action = act.SwitchToWorkspace({
-			name = "server",
-		}),
-	},
-	{
-		key = "3",
-		mods = "SUPER",
-		action = act.SwitchToWorkspace({
-			name = "consoles",
-		}),
-	},
-}
-
 config.term = "xterm-256color"
 
 config.color_schemes = {
@@ -180,5 +123,100 @@ wezterm.on("gui-startup", function(cmd)
 
 	mux.set_active_workspace("editor")
 end)
+
+-- if you are *NOT* lazy-loading smart-splits.nvim (recommended)
+local function is_vim(pane)
+	-- this is set by the plugin, and unset on ExitPre in Neovim
+	return pane:get_user_vars().IS_NVIM == "true"
+end
+
+-- if you *ARE* lazy-loading smart-splits.nvim (not recommended)
+-- you have to use this instead, but note that this will not work
+-- in all cases (e.g. over an SSH connection). Also note that
+-- `pane:get_foreground_process_name()` can have high and highly variable
+-- latency, so the other implementation of `is_vim()` will be more
+-- performant as well.
+-- local function is_vim(pane)
+--   -- This gsub is equivalent to POSIX basename(3)
+--   -- Given "/foo/bar" returns "bar"
+--   -- Given "c:\\foo\\bar" returns "bar"
+--   local process_name = string.gsub(pane:get_foreground_process_name(), '(.*[/\\])(.*)', '%2')
+--   return process_name == 'nvim' or process_name == 'vim'
+-- end
+
+local direction_keys = {
+	h = "Left",
+	j = "Down",
+	k = "Up",
+	l = "Right",
+}
+
+local function split_nav(resize_or_move, key)
+	return {
+		key = key,
+		mods = resize_or_move == "resize" and "META" or "CTRL",
+		action = wezterm.action_callback(function(win, pane)
+			if is_vim(pane) then
+				-- pass the keys through to vim/nvim
+				win:perform_action({
+					SendKey = { key = key, mods = resize_or_move == "resize" and "META" or "CTRL" },
+				}, pane)
+			else
+				if resize_or_move == "resize" then
+					win:perform_action({ AdjustPaneSize = { direction_keys[key], 3 } }, pane)
+				else
+					win:perform_action({ ActivatePaneDirection = direction_keys[key] }, pane)
+				end
+			end
+		end),
+	}
+end
+
+config.keys = {
+	-- move between split panes
+	split_nav("move", "h"),
+	split_nav("move", "j"),
+	split_nav("move", "k"),
+	split_nav("move", "l"),
+	-- resize panes
+	split_nav("resize", "h"),
+	split_nav("resize", "j"),
+	split_nav("resize", "k"),
+	split_nav("resize", "l"),
+	{
+		key = "w",
+		mods = "CTRL",
+		action = act({ CloseCurrentPane = { confirm = true } }),
+	},
+	{
+		key = "k",
+		mods = "SUPER",
+		action = act.Multiple({
+			act.ClearScrollback("ScrollbackAndViewport"),
+			act.SendKey({ key = "L", mods = "CTRL" }),
+		}),
+	},
+	{
+		key = "1",
+		mods = "SUPER",
+		action = act.SwitchToWorkspace({
+			name = "editor",
+		}),
+	},
+	{
+		key = "2",
+		mods = "SUPER",
+		action = act.SwitchToWorkspace({
+			name = "server",
+		}),
+	},
+	{
+		key = "3",
+		mods = "SUPER",
+		action = act.SwitchToWorkspace({
+			name = "consoles",
+		}),
+	},
+}
 
 return config
