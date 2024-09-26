7dad5eb894f8048d392324b886fad6d5f49e3842
Dan Loman <daniel.h.loman@gmail.com>
2024-09-25 22:09:24 -0700

    Simplify/inline colors for wezterm

--- config/.wezterm.lua
+++ config/.wezterm.lua
@@ -3,42 +3,21 @@ local act = wezterm.action
 local mux = wezterm.mux
 local utf8 = require("utf8")
 
-local custom_color_scheme = wezterm.color.get_builtin_schemes()["Aura (Gogh)"]
-
-custom_color_scheme.foreground = "#CBE0F0"
-custom_color_scheme.background = "#011423"
-custom_color_scheme.cursor_fg = "#011423"
-custom_color_scheme.cursor_bg = "#47FF9C"
-custom_color_scheme.ansi = {
-	"#214969",
-	"#E52E2E",
-	"#44FFB1",
-	"#FFE073",
-	"#0FC5ED",
-	"#a277ff",
-	"#24EAF7",
-	"#24EAF7",
-}
-custom_color_scheme.brights = {
-	"#214969",
-	"#E52E2E",
-	"#44FFB1",
-	"#FFE073",
-	"#A277FF",
-	"#a277ff",
-	"#24EAF7",
-	"#24EAF7",
-}
-
 local config = wezterm.config_builder()
 
 config.term = "xterm-256color"
 
-config.color_schemes = {
-	["Aura+"] = custom_color_scheme,
+config.colors = {
+	foreground = "#CBE0F0",
+	background = "#011423",
+	cursor_bg = "#47FF9C",
+	cursor_border = "#47FF9C",
+	cursor_fg = "#011423",
+	selection_bg = "#033259",
+	selection_fg = "#CBE0F0",
+	ansi = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#0FC5ED", "#a277ff", "#24EAF7", "#24EAF7" },
+	brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
 }
-
-config.color_scheme = "Aura+"
 
 config.window_decorations = "RESIZE" -- "INTEGRATED_BUTTONS|RESIZE"
 config.enable_tab_bar = true
