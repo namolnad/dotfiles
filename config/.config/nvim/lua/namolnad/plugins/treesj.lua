3596f3aca4fd08fa079be94d3dc0939862631611
Dan Loman <daniel.h.loman@gmail.com>
2024-10-07 15:46:25 -0700

    Adjust treesj config for extra space in list

--- config/.config/nvim/lua/namolnad/plugins/treesj.lua
+++ config/.config/nvim/lua/namolnad/plugins/treesj.lua
@@ -3,7 +3,21 @@ return {
   keys = { '<space>m', '<space>j', '<space>s' },
   dependencies = { 'nvim-treesitter/nvim-treesitter' },
   config = function()
-    require('treesj').setup {--[[ your config ]]
+    local tsj_utils = require 'treesj.langs.utils'
+    local ruby = require 'treesj.langs.ruby'
+    local options = {
+      join = {
+        space_in_brackets = false,
+      },
+    }
+
+    local langs = {
+      ruby = tsj_utils.merge_preset(ruby, {
+        array = tsj_utils.set_preset_for_list(options),
+      }),
+    }
+    require('treesj').setup {
+      langs = langs,
     }
   end,
 }
