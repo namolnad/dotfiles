e4d5ea2535cee786d304daaca904872e746f763e
Dan Loman <daniel.h.loman@gmail.com>
2024-11-13 14:38:12 -0800

    Start to modify avante bindings

--- config/.config/nvim/lua/namolnad/plugins/avante.lua
+++ config/.config/nvim/lua/namolnad/plugins/avante.lua
@@ -43,4 +43,46 @@ return {
       ft = { "markdown", "Avante" },
     },
   },
+  setup = function()
+    -- This setup function is called once the user has installed the plugin
+    -- If the plugin is lazy loaded, this function is called when the plugin is loaded
+    -- This is a good place to put your key mappings
+    -- For more examples, see the `setup` function in the `examples` directory
+    local avante = require("avante")
+
+    avante.setup({
+      mappings = {
+        --- @class AvanteConflictMappings
+        diff = {
+          ours = "co",
+          theirs = "ct",
+          all_theirs = "ca",
+          both = "cb",
+          cursor = "cc",
+          next = "]x",
+          prev = "[x",
+        },
+        suggestion = {
+          accept = "<C-y>", -- <M-l> is former
+          next = "<M-]>",
+          prev = "<M-[>",
+          dismiss = "<C-]>",
+        },
+        jump = {
+          next = "]]",
+          prev = "[[",
+        },
+        submit = {
+          normal = "<CR>",
+          insert = "<C-s>",
+        },
+        sidebar = {
+          apply_all = "<leader>A",    -- A is former
+          apply_cursor = "<leader>a", -- a is former
+          switch_windows = "<Tab>",
+          reverse_switch_windows = "<S-Tab>",
+        },
+      },
+    })
+  end,
 }
