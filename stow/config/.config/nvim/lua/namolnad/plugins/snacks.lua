28f35631acfee05eac75bccaf2f73a4ac0cc5b83
Dan Loman <daniel.h.loman@gmail.com>
2024-12-21 16:00:24 -0800

    Add to snacks dashboard

--- stow/config/.config/nvim/lua/namolnad/plugins/snacks.lua
+++ stow/config/.config/nvim/lua/namolnad/plugins/snacks.lua
@@ -5,7 +5,40 @@ return {
   ---@type snacks.Config
   opts = {
     bigfile = { enabled = true },
-    dashboard = { enabled = true },
+    dashboard = {
+      enabled = true,
+      sections = {
+        { section = "header" },
+        -- {
+        --   pane = 2,
+        --   section = "terminal",
+        --   cmd =
+        --   "chafa ~/.local/share/nvim/dashboard-background.png --format symbols --symbols vhalf --size 30x10 --stretch; sleep 0.1",
+        --   -- cmd = "imgcat ~/.local/share/nvim/dashboard-background.png --width 60 --depth iterm2",
+        --   -- cmd = "ascii-image-converter --color --braille ~/.local/share/nvim/dashboard-background.png",
+        --   height = 10,
+        --   padding = 1,
+        -- },
+        { section = "keys", gap = 1, padding = 1 },
+        { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+        { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+        -- {
+        --   pane = 2,
+        --   icon = " ",
+        --   title = "Git Status",
+        --   section = "terminal",
+        --   enabled = function()
+        --     return Snacks.git.get_root() ~= nil
+        --   end,
+        --   cmd = "git status --short --branch --renames",
+        --   height = 5,
+        --   padding = 1,
+        --   ttl = 5 * 60,
+        --   indent = 3,
+        -- },
+        { section = "startup" },
+      },
+    },
     indent = {},
     notifier = {
       enabled = true,
