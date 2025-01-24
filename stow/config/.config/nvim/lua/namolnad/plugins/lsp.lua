fcb9465fdb759b2bb82e58892287081f3cb35b84
Dan Loman <daniel.h.loman@gmail.com>
2025-01-23 16:58:33 -0800

    Move lsp folding stuff to lsp

--- stow/config/.config/nvim/lua/namolnad/plugins/lsp.lua
+++ stow/config/.config/nvim/lua/namolnad/plugins/lsp.lua
@@ -184,13 +184,21 @@ return { -- LSP Configuration & Plugins
 
     local lspconfig = require 'lspconfig'
     require('mason-lspconfig').setup {
+      automatic_installation = true,
+      ensure_installed = servers,
       handlers = {
         function(server_name)
           local server = servers[server_name] or {}
           -- This handles overriding only values explicitly passed
           -- by the server configuration above. Useful when disabling
           -- certain features of an LSP (for example, turning off formatting for tsserver)
-          server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
+          server.capabilities = vim.tbl_deep_extend(
+            'force',
+            {},
+            capabilities,
+            server.capabilities or {},
+            { textDocument = { foldingRange = { dynamicRegistration = false, lineFoldingOnly = true } } }
+          )
           lspconfig[server_name].setup(server)
         end,
 
--- stow/config/.config/nvim/lua/namolnad/plugins/ufo.lua
+++ stow/config/.config/nvim/lua/namolnad/plugins/ufo.lua
@@ -1,6 +1,7 @@
 return {
   'kevinhwang91/nvim-ufo',
   dependencies = { 'kevinhwang91/promise-async' },
+  event = "VeryLazy",
   config = function()
     -- require('ufo').setup()
     vim.o.foldcolumn = '1' -- '0' is not bad
@@ -12,18 +13,19 @@ return {
     vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
     vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
 
-    local capabilities = vim.lsp.protocol.make_client_capabilities()
-    capabilities.textDocument.foldingRange = {
-      dynamicRegistration = false,
-      lineFoldingOnly = true
-    }
-    local language_servers = require("lspconfig").util.available_servers() -- or list servers manually like {'gopls', 'clangd'}
-    for _, ls in ipairs(language_servers) do
-      require('lspconfig')[ls].setup({
-        capabilities = capabilities
-        -- you can add other fields for setting up lsp server in this table
-      })
-    end
+    -- Handled in lsp.lua
+    -- local capabilities = vim.lsp.protocol.make_client_capabilities()
+    -- capabilities.textDocument.foldingRange = {
+    --   dynamicRegistration = false,
+    --   lineFoldingOnly = true
+    -- }
+    -- local language_servers = { 'ruby_lsp', }
+    -- for _, ls in ipairs(language_servers) do
+    --   require('lspconfig')[ls].setup({
+    --     capabilities = capabilities
+    --     -- you can add other fields for setting up lsp server in this table
+    --   })
+    -- end
     require('ufo').setup()
   end
 }
