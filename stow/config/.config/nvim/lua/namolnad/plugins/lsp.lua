caad5afaa99a015eb26c1585b57774b15e454451
Dan Loman <daniel.h.loman@gmail.com>
2025-01-24 14:29:50 -0800

    Fix LSP stuff (work outside of Mason)

--- stow/config/.config/nvim/lua/namolnad/plugins/lsp.lua
+++ stow/config/.config/nvim/lua/namolnad/plugins/lsp.lua
@@ -16,35 +16,6 @@ return { -- LSP Configuration & Plugins
     'saghen/blink.cmp',
   },
   config = function()
-    -- Brief aside: **What is LSP?**
-    --
-    -- LSP is an initialism you've probably heard, but might not understand what it is.
-    --
-    -- LSP stands for Language Server Protocol. It's a protocol that helps editors
-    -- and language tooling communicate in a standardized fashion.
-    --
-    -- In general, you have a "server" which is some tool built to understand a particular
-    -- language (such as `gopls`, `lua_ls`, `rust_analyzer`, etc.). These Language Servers
-    -- (sometimes called LSP servers, but that's kind of like ATM Machine) are standalone
-    -- processes that communicate with some "client" - in this case, Neovim!
-    --
-    -- LSP provides Neovim with features like:
-    --  - Go to definition
-    --  - Find references
-    --  - Autocompletion
-    --  - Symbol Search
-    --  - and more!
-    --
-    -- Thus, Language Servers are external tools that must be installed separately from
-    -- Neovim. This is where `mason` and related plugins come into play.
-    --
-    -- If you're wondering about lsp vs treesitter, you can check out the wonderfully
-    -- and elegantly composed help section, `:help lsp-vs-treesitter`
-
-    --  This function gets run when an LSP attaches to a particular buffer.
-    --    That is to say, every time a new file is opened that is associated with
-    --    an lsp (for example, opening `main.rs` is associated with `rust_analyzer`) this
-    --    function will be executed to configure the current buffer
     vim.api.nvim_create_autocmd('LspAttach', {
       group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
       callback = function(event)
@@ -100,12 +71,6 @@ return { -- LSP Configuration & Plugins
 
         -- Format the current buffer.
         map('<leader>f', vim.lsp.buf.format, '[F]ormat')
-
-        -- The following two autocommands are used to highlight references of the
-        -- word under your cursor when your cursor rests there for a little while.
-        --    See `:help CursorHold` for information about when this is executed
-        --
-        -- When you move your cursor, the highlights will be cleared (the second autocommand).
         local client = vim.lsp.get_client_by_id(event.data.client_id)
         if client and client.server_capabilities.documentHighlightProvider then
           vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
@@ -121,116 +86,60 @@ return { -- LSP Configuration & Plugins
       end,
     })
 
-    -- LSP servers and clients are able to communicate to each other what features they support.
-    --  By default, Neovim doesn't support everything that is in the LSP specification.
-    --  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
-    --  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
     local capabilities = vim.lsp.protocol.make_client_capabilities()
-    capabilities = vim.tbl_deep_extend('force', capabilities, require('blink.cmp').get_lsp_capabilities())
+    capabilities = vim.tbl_deep_extend(
+      'force',
+      capabilities,
+      require('blink.cmp').get_lsp_capabilities(),
+      { textDocument = { foldingRange = { dynamicRegistration = false, lineFoldingOnly = true } } }
+    )
 
-    -- capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
-
-    -- Enable the following language servers
-    --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
-    --
-    --  Add any additional override configuration in the following tables. Available keys are:
-    --  - cmd (table): Override the default command used to start the server
-    --  - filetypes (table): Override the default list of associated filetypes for the server
-    --  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
-    --  - settings (table): Override the default settings passed when initializing the server.
-    --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
+    local rbenv_root = vim.fn.trim(vim.fn.system('rbenv root'))
     local servers = {
       lua_ls = {
-        -- cmd = {...},
-        -- filetypes = { ...},
-        -- capabilities = {},
         settings = {
           Lua = {
             completion = {
               callSnippet = 'Replace',
             },
-            -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-            -- diagnostics = { disable = { 'missing-fields' } },
+            diagnostics = {
+              disable = { 'missing-fields' },
+              globals = { 'vim', 'it', 'describe', 'before_each', 'after_each' },
+            },
           },
         },
       },
-      ruby_lsp = {
-        -- cmd = {...},
-        -- filetypes = { ...},
-        -- capabilities = {},
-      },
+      ts_ls = {},
+      stylua = {},
+      prettier = {},
     }
 
-    -- Ensure the servers and tools above are installed
-    --  To check the current status of installed tools and/or manually install
-    --  other tools, you can run
-    --    :Mason
-    --
-    --  You can press `g?` for help in this menu.
+
+    local lspconfig = require('lspconfig')
+
+    -- Several Ruby LSPs use custom cmd paths as the LSPs are included in the Gemfile,
+    -- so we need to set them up manually, outside of Mason. Otherwise Mason will
+    -- use the binary it installs vs the one from the Gemfile (and ruby versions might not match)
+    lspconfig.ruby_lsp.setup {
+      cmd = { rbenv_root .. '/shims/ruby-lsp' },
+      capabilities = capabilities,
+    }
+    lspconfig.rubocop.setup {
+      cmd = { 'bundle', 'exec', 'rubocop', '--lsp' },
+      capabilities = capabilities,
+    }
+
     require('mason').setup()
-
-    -- remove ruby_lsp from ensure_installed; We want to install from bundle
-    local ensure_installed = vim.tbl_filter(
-      function(server) return server ~= "ruby_lsp" end,
-      vim.tbl_keys(servers or {})
-    )
-    vim.list_extend(ensure_installed, {
-      'stylua', -- Used to format Lua code
-      'rubocop',
-      'ts_ls',
-      'prettier',
+    require('mason-tool-installer').setup { ensure_installed = vim.tbl_keys(servers) }
+    require('mason-lspconfig').setup({
+      automatic_installation = false,
+      ensure_installed = {}, -- Mason tool installer will handle this and allow either Mason or lspconfig names
+      -- handlers = {
+      --   function(server_name)
+      --     lspconfig[server_name].setup {}
+      --   end,
+      -- }
     })
-    require('mason-tool-installer').setup { ensure_installed = ensure_installed }
-
-    local lspconfig = require 'lspconfig'
-    require('mason-lspconfig').setup {
-      automatic_installation = true,
-      ensure_installed = servers,
-      handlers = {
-        function(server_name)
-          local server = servers[server_name] or {}
-          -- This handles overriding only values explicitly passed
-          -- by the server configuration above. Useful when disabling
-          -- certain features of an LSP (for example, turning off formatting for tsserver)
-          server.capabilities = vim.tbl_deep_extend(
-            'force',
-            {},
-            capabilities,
-            server.capabilities or {},
-            { textDocument = { foldingRange = { dynamicRegistration = false, lineFoldingOnly = true } } }
-          )
-          lspconfig[server_name].setup(server)
-        end,
-
-        ['ruby_lsp'] = function()
-          local rbenv_root = vim.fn.trim(vim.fn.system('rbenv root'))
-          lspconfig.ruby_lsp.setup {
-            capabilities = capabilities,
-            cmd = { rbenv_root .. '/shims/ruby-lsp' }
-          }
-        end,
-
-        ['rubocop'] = function()
-          lspconfig.rubocop.setup {
-            cmd = { 'bundle', 'exec', 'rubocop', '--lsp' },
-            capabilities = capabilities,
-          }
-        end,
-
-        ['lua_ls'] = function()
-          lspconfig.lua_ls.setup {
-            capabilities = capabilities,
-            settings = {
-              Lua = {
-                diagnostics = {
-                  globals = { 'vim', 'it', 'describe', 'before_each', 'after_each' },
-                },
-              },
-            },
-          }
-        end,
-      },
-    }
 
     vim.diagnostic.config {
       -- update_in_insert = true,
