c5a1549b9695ea47d56ac0c0bff9398c50e599e2
Dan Loman <daniel.h.loman@gmail.com>
2025-08-02 09:52:32 -0700

    Remove extra flash config for now

--- stow/config/.config/nvim/lua/user/plugins/flash.lua
+++ stow/config/.config/nvim/lua/user/plugins/flash.lua
@@ -4,12 +4,12 @@ return {
   ---@type Flash.Config
   opts = {
     modes = {
-      search = {
-        enabled = true
-      },
-      char = {
-        jump_labels = true
-      }
+      -- search = {
+      --   enabled = true
+      -- },
+      -- char = {
+      --   jump_labels = true
+      -- }
     }
 
   },
