4bd2ea3f15350613da48f6495f7127bb4e6b1019
Dan Loman <daniel.h.loman@gmail.com>
2025-08-03 15:56:58 -0700

    Adjust unimpaired remap

--- stow/config/.config/nvim/lua/user/plugins/unimpaired.lua
+++ stow/config/.config/nvim/lua/user/plugins/unimpaired.lua
@@ -1,7 +1,11 @@
 return {
   'tummetott/unimpaired.nvim',
   event = 'VeryLazy',
-  opts = {
-    -- add options here if you wish to override the default settings
-  },
+  config = function()
+    require('unimpaired').setup()
+
+    -- Override visual mappings to use same logic as J/K remaps
+    vim.keymap.set('v', ']e', ":m '>+1<CR>gv=gv", { desc = 'Move visual selection down' })
+    vim.keymap.set('v', '[e', ":m '<-2<CR>gv=gv", { desc = 'Move visual selection up' })
+  end,
 }
