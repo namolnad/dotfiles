7f97fce0bd15afc97116b0c2d26d1031791e400e
Dan Loman <daniel.h.loman@gmail.com>
2025-08-04 16:45:38 -0700

    Test exec

--- test-input.sh
+++ test-input.sh
@@ -3,6 +3,7 @@
 echo "Testing interactive input for both direct execution and piped execution..."
 echo "Press 'y' to continue:"
 
+exec < /dev/tty
 while : ; do
     read -n 1 k
     if [[ $k = 'y' ]] ; then
