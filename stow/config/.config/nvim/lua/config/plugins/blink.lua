5358ecfa347eed0a74baccc2673f9379ca269408
Dan Loman <daniel.h.loman@gmail.com>
2025-08-10 18:16:39 -0700

    Revert "Try to include avante again for autosuggestions"
    This reverts commit 5d0101a00674e17b367719d2b678b8e11d0c0e6f.

--- stow/config/.config/nvim/lua/config/plugins/blink.lua
+++ stow/config/.config/nvim/lua/config/plugins/blink.lua
@@ -1,13 +1,4 @@
 return {
-    {
-    'saghen/blink.compat',
-    -- use v2.* for blink.cmp v1.*
-    version = '2.*',
-    -- lazy.nvim will automatically load the plugin when it's required by blink.cmp
-    lazy = true,
-    -- make sure to set opts so that lazy.nvim calls blink.compat's setup
-    opts = {},
-  },
   {
     'saghen/blink.cmp',
     dependencies = {
@@ -20,48 +11,13 @@ return {
         use_nvim_cmp_as_default = true,
         nerd_font_variant = 'mono'
       },
+
       -- default list of enabled providers defined so that you can extend it
       -- elsewhere in your config, without redefining it, via `opts_extend`
       sources = {
-        default = {
-          'lsp',
-          'path',
-          'snippets',
-          'buffer',
-          'avante_commands',
-          'avante_mentions',
-          'avante_shortcuts',
-          'avante_files',
-        },
+        default = { 'lsp', 'path', 'snippets', 'buffer' },
         -- optionally disable cmdline completions
         -- cmdline = {},
-        providers = {
-          avante_commands = {
-            name = 'avante_commands',
-            module = 'blink.compat.source',
-            score_offset = 90, -- show at a higher priority than lsp
-            opts = {},
-          },
-          avante_files = {
-            name = 'avante_files',
-            module = 'blink.compat.source',
-            score_offset = 100, -- show at a higher priority than lsp
-            opts = {},
-          },
-          avante_mentions = {
-            name = 'avante_mentions',
-            module = 'blink.compat.source',
-            score_offset = 1000, -- show at a higher priority than lsp
-            opts = {},
-          },
-          avante_shortcuts = {
-            name = 'avante_shortcuts',
-            module = 'blink.compat.source',
-            score_offset = 1000, -- show at a higher priority than lsp
-            opts = {},
-          }
-        },
-
       },
       signature = { enabled = true }
     },
